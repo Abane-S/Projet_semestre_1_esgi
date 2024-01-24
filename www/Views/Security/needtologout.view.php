a447c63544b4f6cd194d3b38d974ac9fc820d380
Botan YILMAZ <byilmaz1@myges.fr>
2024-01-24 14:52:49 +0100

    fix login page

---
+++ www/Views/Security/needtologout.view.php
@@ -0,0 +1,6 @@
+<div class="div_input">
+    <h2>Déconnexion ?</h2>
+
+    <span>Vous avez besoin de vous déconnecter pour accéder à cette page.<br>Merci de vous déconnecter.</span>
+    <a href="/logout">Déconnexion</a>
+</div>
